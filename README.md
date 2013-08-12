ffc6c3f1a72949765e97876418cf0a575c1553d1
Fumihiro Itoh <fmhrit@gmail.com>
2013-08-12 22:44:18 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Devel::CycleUse - It's new $module
+
+# SYNOPSIS
+
+    use Devel::CycleUse;
+
+# DESCRIPTION
+
+Devel::CycleUse is ...
+
+# LICENSE
+
+Copyright (C) Fumihiro Itoh.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Fumihiro Itoh <fmhrit@gmail.com>
---
+++ lib/Devel/CycleUse.pm
@@ -0,0 +1,39 @@
+package Devel::CycleUse;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Devel::CycleUse - It's new $module
+
+=head1 SYNOPSIS
+
+    use Devel::CycleUse;
+
+=head1 DESCRIPTION
+
+Devel::CycleUse is ...
+
+=head1 LICENSE
+
+Copyright (C) Fumihiro Itoh.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Fumihiro Itoh E<lt>fmhrit@gmail.comE<gt>
+
+=cut
+
